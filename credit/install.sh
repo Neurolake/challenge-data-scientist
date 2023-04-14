643bc515377f86915b3028a5daca70092786dff2
Guilherme Boaviagem <guilherme.boaviagem@neurotech.com.br>
2023-04-14 18:00:50 -0300

    Add setup and requirements for Credit.

---
+++ credit/install.sh
@@ -0,0 +1,3 @@
+python -m pip install -r requirements.txt
+conda install -c conda-forge jupyterhub  # installs jupyterhub and proxy
+conda install jupyterlab notebook  # needed if running the notebook servers in the same environment
